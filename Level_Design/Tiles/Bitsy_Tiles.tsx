0f1835ae900c3c1e8d95817360b0d197aab205e2
gitguyy <gilmoreschneider34@gmail.com>
2025-10-03 21:55:10 +0200

    some edits

---
+++ Level_Design/Tiles/Bitsy_Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Bitsy_Tiles" tilewidth="16" tileheight="16" tilecount="16" columns="4">
+ <image source="../../../../Gil/jamsepticeye-1/Assets/Visuals/export_bits.aseprite" width="64" height="64"/>
+</tileset>
